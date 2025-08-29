58e2d29ad130b86f7144608b341d49a97a02ac50
Bmartineezz <165804199+Bmartineezz@users.noreply.github.com>
2025-08-29 11:16:06 -0300

    Update TicketMachine.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -14,7 +14,7 @@ public class TicketMachine {
     protected int saldo;
     protected int[] papelMoeda = {2, 5, 10, 20, 50, 100};
 
-    public TicketMachine(int valor) {
+    public TicketMachine(int valor) { 
         this.valor = valor;
         this.saldo = 0;
     }
@@ -22,7 +22,7 @@ public class TicketMachine {
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
         for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[1] == quantia) {
+            if (papelMoeda[1] == quantia) { //mudar a comparação para  comparar com o array todo e não só com uma posição
                 achou = true;
             }
         }
@@ -37,16 +37,19 @@ public class TicketMachine {
     }
 
     public Iterator<Integer> getTroco() {
-        return null;
+        return null; // retornar variavel troco e não vazio   
     }
 
     public String imprimir() throws SaldoInsuficienteException {
         if (saldo < valor) {
             throw new SaldoInsuficienteException();
-        }
+        }// deveria possuir um if saldo > valor e fazer o calculo de saldo - valor  
         String result = "*****************\n";
         result += "*** R$ " + saldo + ",00 ****\n";
         result += "*****************\n";
+        //deveria imprimir que o bilhete foi impresso também
+         
         return result;
     }
 }
+
